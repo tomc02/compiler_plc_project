225e9f17d8db9fb7661c2798da4fd6344a474358
Tomas Cienciala <tomas.cienciala.st1@vsb.cz>
2024-02-26 11:12:08 +0100

    Final update

--- cv1.py
+++ cv1.py
@@ -1,32 +1,18 @@
-def tokenize(expression):
-    tokens = []
-    number = ''
-    for char in expression:
-        if char.isdigit():
-            number += char
-        else:
-            if number != '':
-                tokens.append(int(number))
-                number = ''
-            if char in "+-*/()":
-                tokens.append(char)
-    if number != '':
-        tokens.append(int(number))
-    return tokens
-
 def shunting_yard(tokens):
     priority = {'+': 1, '-': 1, '*': 2, '/': 2}
-    output_queue = []
+    output = []
     operator_stack = []
     last_token = None
     
     for token in tokens:
         if isinstance(token, int):
-            output_queue.append(token)
+            if last_token == ')':
+                return "ERROR"  
+            output.append(token)
         elif token in "+-*/":
             while (operator_stack and operator_stack[-1] in "+-*/" and
                    priority[operator_stack[-1]] >= priority[token]):
-                output_queue.append(operator_stack.pop())
+                output.append(operator_stack.pop())
             operator_stack.append(token)
         elif token == '(':
             if last_token and isinstance(last_token, int):
@@ -34,14 +20,14 @@ def shunting_yard(tokens):
             operator_stack.append(token)
         elif token == ')':
             while operator_stack and operator_stack[-1] != '(':
-                output_queue.append(operator_stack.pop())
-            operator_stack.pop()  # Pop the '('
+                output.append(operator_stack.pop())
+            operator_stack.pop()
         last_token = token
     
     while operator_stack:
-        output_queue.append(operator_stack.pop())
+        output.append(operator_stack.pop())
     
-    return output_queue
+    return output
 
 def evaluate(expression):
     stack = []
@@ -62,12 +48,28 @@ def evaluate(expression):
                 stack.append(a // b)
     return stack[0] if stack else "ERROR"
 
-def interpret_expressions(inputs):
+def parse(expression):
+    tokens = []
+    number = ''
+    for char in expression:
+        if char.isdigit():
+            number += char
+        else:
+            if number != '':
+                tokens.append(int(number))
+                number = ''
+            if char in "+-*/()":
+                tokens.append(char)
+    if number != '':
+        tokens.append(int(number))
+    return tokens
+
+def solve_expressions(inputs):
     results = []
     for expression in inputs:
         try:
-            tokens = tokenize(expression)
-            postfix = shunting_yard(tokens)
+            parsed = parse(expression)
+            postfix = shunting_yard(parsed)
             result = evaluate(postfix)
             results.append(result)
         except Exception as e:
@@ -81,4 +83,4 @@ if __name__ == "__main__":
     while count > 0:
         inputs.append(str(input()))
         count -= 1
-    print(interpret_expressions(inputs))
+    print(solve_expressions(inputs))
