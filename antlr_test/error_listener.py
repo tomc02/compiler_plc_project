29d20026c8a8d6133263dbacbe53938f6981f3ed
Tomas Cienciala <tomas.cienciala.st1@vsb.cz>
2024-04-05 15:28:19 +0200

    Folders reorganization

--- antlr_test/error_listener.py
+++
@@ -1,8 +0,0 @@
-# create error listener for antlr4
-
-def create_error_listener():
-    class ErrorListener(ConsoleErrorListener):
-        def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-            raise Exception(f"line {line}:{column} {msg}")
-
-    return ErrorListener()
