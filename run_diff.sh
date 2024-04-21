9a1fbda319b2fd29ea50c2073d31fc9f5f3b044b
Tomas Cienciala <tomas.cienciala.st1@vsb.cz>
2024-04-21 20:11:08 +0200

    Removed useless script

--- run_diff.sh
+++
@@ -1,3 +0,0 @@
-kdiff3 expamle_outputs/PLC_t1.out.txt  outputs/in1.txt > /dev/null 2>&1 &
-kdiff3 expamle_outputs/PLC_t2.out.txt  outputs/in2.txt > /dev/null 2>&1 &
-kdiff3 expamle_outputs/PLC_t3.out.txt  outputs/in3.txt > /dev/null 2>&1 &
